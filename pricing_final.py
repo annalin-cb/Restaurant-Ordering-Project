78c1ce384844c4772435d1cfa88d1a6c03dbf892
annalin-cb <penbred14@gmail.com>
2026-03-19 08:39:40 -0400

    Updated

--- pricing_final.py
+++ pricing_final.py
@@ -24,25 +24,26 @@ class Pricing: #taxes for food and drink are set to 6% and 8% respectively
         self.discounts = []
 
     def add_discount(self, discount): #allows discount amount to be added
-        self.discounts.append(self.discount)
+        self.discounts.append(discount)
 
     def calculate_subtotal(self, items_ordered): #calculates the order's subtotal
         return sum(item.item.get_price() * item.quantity for item in items_ordered)
         
     def apply_tax(self, items_ordered): #appplies tax based on whether an item is within the food category or drink category
         tax = 0
-        item = items_ordered.item
-        qty = items_ordered.quantity
-        if item.category == "food":
-            tax += item.get_price() * qty * Pricing.food_tax
-        elif item.category == "drink":
-            tax += item.get_price() * qty * Pricing.drink_tax
+        for order_item in items_ordered:
+            item = order_item.item
+            qty = order_item.quantity
+            if item.category == "food":
+                tax += item.get_price() * qty * Pricing.food_tax
+            elif item.category == "drink":
+                tax += item.get_price() * qty * Pricing.drink_tax
         return tax
 
     def calculate_discounts(self, subtotal): #uses the subtotal and discount list to find the total discount
         total_discount = 0
         for a_discount in discounts:
-            total_discount += a_discount * subtotal
+            total_discount += subtotal - a_discount
         return total_discount
     def calculate_tip(self, amount): #applies a tip to the order based on its subtotal
         return amount * self.tip_percent
@@ -50,8 +51,8 @@ class Pricing: #taxes for food and drink are set to 6% and 8% respectively
     def calculate_total(self, items_ordered): #calculates and returns the order's full total, now including all discounts, tips, and taxes
         subtotal = self.calculate_subtotal(items_ordered)
         discount = self.calculate_discounts(subtotal)
+        after_discount = subtotal - discount
         tax = self.apply_tax(items_ordered)
-        after_discount = subtotal - discount - tip
         tip = self.calculate_tip(after_discount)
         final_total = after_discount + tax + tip
         return f"Calculating order...\nSubtotal: {subtotal} \nDiscount: {discount} \nTax: {tax} \nTip: {tip} \n.......... \nTotal: {final_total}"
